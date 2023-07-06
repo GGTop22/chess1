e8fcbc76efd1df915da4a49adfd5f65b509e707e
GGTop22 <tikhomgonzo2203@gmail.com>
2023-07-06 17:22:20 +0200

    zdelal varianty pryzkov konya

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def symbolsToIntCoord(k: str)->(int, int):
-    a = ord(k[0])-ord('A')+1
+def symbolsToIntCoord(k: str) -> (int, int):
+    a = ord(k[0]) - ord('A') + 1
     b = int(k[1])
-    return (a,b)
+    return (a, b)
+
 
 move = input("vvedite hod konёm")
 move = move.upper()
@@ -13,18 +14,13 @@ if (len(move)) == 5:
     if k2[0] not in ["A", "B", "C", "D", "E", "F", "G", "H"] or k2[1] not in ["1", "2", "3", "4", "5", "6", "7", "8"]:
         print("ERROR")
 
-    point1 =     symbolsToIntCoord(k1)
-    point2 =     symbolsToIntCoord(k2)
-    print (point1, point2)
+    point1 = symbolsToIntCoord(k1)
+    point2 = symbolsToIntCoord(k2)
+    print(point1, point2)
 else:
     # print(k1, k2 ,'YES')
     print('ERROR')
 # проверку правильности хода нужно переделать!
-if move in (k1, k2):
-    print(k1, k2, 'YES')
-else:
-    print(k1, k2, 'NO')
-
 
 # verhnee pravo /2 vverh,1 vpravo
 # verhnee levo /2 vverh,-1 vpravo
@@ -34,3 +30,27 @@ else:
 # niznie pravo/-2 vverh,1 vpravo
 # leviy verh/1 vverh,-2 vpravo
 # leviy niz/-1 vverh,-2 vpravo
+
+if point2[0] == point1[0] + 1 and point2[1] == point1[1] + 2:
+    print('verhnee pravo')
+
+if point2[0] == point1[0] - 1 and point2[1] == point1[1] + 2:
+    print('verhnee levo')
+
+if point2[0] == point1[0] + 2 and point2[1] == point1[1] + 1:
+    print('praviy verh')
+
+if point2[0] == point1[0] + 2 and point2[1] == point1[1] - 1:
+    print('praviy niz')
+
+if point2[0] == point1[0] - 2 and point2[1] == point1[1] - 1:
+    print('niznie levo')
+
+if point2[0] == point1[0] + 1 and point2[1] == point1[1] - 2:
+    print('niznie pravo')
+
+if point2[0] == point1[0] - 2 and point2[1] == point1[1] + 1:
+    print('leviy verh')
+
+if point2[0] == point1[0] - 2 and point2[1] == point1[1] - 1:
+    print('leviy niz')
