e7abd16cf77e37fca99ac753a6bb78a6459fd9e6
alkaz07 <alkaz07@yandex,ru>
2023-07-06 18:15:15 +0300

    + функция проверки хода конем без разделения на варианты прыжка

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def symbolsToIntCoord(k: str)->(int, int):
-    a = ord(k[0])-ord('A')+1
+def symbolsToIntCoord(k: str) -> (int, int):
+    a = ord(k[0]) - ord('A') + 1
     b = int(k[1])
-    return (a,b)
+    return (a, b)
+
+
+def isKnightJunp(p1: (int, int), p2: (int, int)) -> bool:
+    return (abs(p1[0] - p2[0]) == 2 and abs(p1[1] - p2[1]) == 1) or (abs(p1[0] - p2[0]) == 1 and abs(p1[1] - p2[1]) == 2)
+
 
 move = input("vvedite hod konёm")
 move = move.upper()
@@ -13,17 +18,17 @@ if (len(move)) == 5:
     if k2[0] not in ["A", "B", "C", "D", "E", "F", "G", "H"] or k2[1] not in ["1", "2", "3", "4", "5", "6", "7", "8"]:
         print("ERROR")
 
-    point1 =     symbolsToIntCoord(k1)
-    point2 =     symbolsToIntCoord(k2)
-    print (point1, point2)
+    point1 = symbolsToIntCoord(k1)
+    point2 = symbolsToIntCoord(k2)
+    print(point1, point2)
+
+    if (isKnightJunp(point1, point2)):
+        print('YES')
+    else:
+        print('NO')
 else:
     # print(k1, k2 ,'YES')
     print('ERROR')
-# проверку правильности хода нужно переделать!
-if move in (k1, k2):
-    print(k1, k2, 'YES')
-else:
-    print(k1, k2, 'NO')
 
 
 # verhnee pravo /2 vverh,1 vpravo
