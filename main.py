100cda645e1f12f9c9f0c8a279690087c3873d49
GGTop22 <tikhomgonzo2203@gmail.com>
2023-07-06 16:35:25 +0200

    dobavil varianty pryzkov konya

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ move = input("vvedite hod konёm")
 move = move.upper()
 if (len(move)) == 5:
     k1, k2 = move.split('-')
-    print(k1, k2 )
+    print(k1, k2)
     if k1[0] not in ["A", "B", "C", "D", "E", "F", "G", "H"] or k1[1] not in ["1", "2", "3", "4", "5", "6", "7", "8"]:
         print("ERROR")
     if k2[0] not in ["A", "B", "C", "D", "E", "F", "G", "H"] or k2[1] not in ["1", "2", "3", "4", "5", "6", "7", "8"]:
         print("ERROR")
 else:
-    #print(k1, k2 ,'YES')
+    # print(k1, k2 ,'YES')
     print('ERROR')
-#проверку правильности хода нужно переделать!
-if move in (k1,k2 ):
-    print(k1, k2 ,'YES')
+# проверку правильности хода нужно переделать!
+if move in (k1, k2):
+    print(k1, k2, 'YES')
 else:
-    print(k1, k2 ,'NO')
+    print(k1, k2, 'NO')
+# verhnee pravo /2 vverh,1 vpravo
+# verhnee levo /2 vverh,-1 vpravo
+# praviy verh /1 vverh,2 vpravo
+# praviy niz/-1 vverh,2 vpravo
+# niznie levo/-2 vverh,-1 vpravo
+# niznie pravo/-2 vverh,1 vpravo
+# leviy verh/1 vverh,-2 vpravo
+# leviy niz/-1 vverh,-2 vpravo
