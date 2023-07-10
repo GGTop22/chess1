e64164035fc41de89831b7d89d0c23ea45673200
GGTop22 <tikhomgonzo2203@gmail.com>
2023-07-10 16:59:57 +0200

    passes tests from 1 to 21
    have fixed the '-' presence check
    
    have fixed the coordinate length check
    
    have fixed output.write
    
    deleted unnecessary entries

--- main.py
+++ main.py
@@ -4,40 +4,42 @@ def symbolsToIntCoord(k: str) -> (int, int):
     return (a, b)
 
 
-def isKnightJunp(p1: (int, int), p2: (int, int)) -> bool:
+def isKnightJump(p1: (int, int), p2: (int, int)) -> bool:
     return (abs(p1[0] - p2[0]) == 2 and abs(p1[1] - p2[1]) == 1) or (
             abs(p1[0] - p2[0]) == 1 and abs(p1[1] - p2[1]) == 2)
 
 
-move = input("vvedite hod konёm")
-move = move.upper()
+# move = input("vvedite hod konёm")
+# move = move.upper()
 
 
 def hodKonem(move):
+    move = move.upper()
     if (len(move)) == 5:
+        if move[2] != '-':
+            return 'ERROR'
         k1, k2 = move.split('-')
-        print(k1, k2)
+        if len(k1) != 2 or len(k2) != 2:
+            return 'ERROR'
+
         if k1[0] not in ["A", "B", "C", "D", "E", "F", "G", "H"] or k1[1] not in ["1", "2", "3", "4", "5", "6", "7",
                                                                                   "8"]:
-            print("ERROR")
+            return "ERROR"
         if k2[0] not in ["A", "B", "C", "D", "E", "F", "G", "H"] or k2[1] not in ["1", "2", "3", "4", "5", "6", "7",
                                                                                   "8"]:
-            print("ERROR")
+            return "ERROR"
 
         point1 = symbolsToIntCoord(k1)
         point2 = symbolsToIntCoord(k2)
         print(point1, point2)
-        point1 = symbolsToIntCoord(k1)
-        point2 = symbolsToIntCoord(k2)
-        print(point1, point2)
 
-        if (isKnightJunp(point1, point2)):
-            print('YES')
+        if (isKnightJump(point1, point2)):
+            return 'YES'
         else:
-            print('NO')
+            return 'NO'
     else:
         # print(k1, k2 ,'YES')
-        print('ERROR')
+        return 'ERROR'
 
     # verhnee pravo /2 vverh,1 vpravo
     # verhnee levo /2 vverh,-1 vpravo
@@ -48,31 +50,32 @@ def hodKonem(move):
     # leviy verh/1 vverh,-2 vpravo
     # leviy niz/-1 vverh,-2 vpravo
 
-    if point2[0] == point1[0] + 1 and point2[1] == point1[1] + 2:
-        print('verhnee pravo')
 
-    if point2[0] == point1[0] - 1 and point2[1] == point1[1] + 2:
-        print('verhnee levo')
+# if point2[0] == point1[0] + 1 and point2[1] == point1[1] + 2:
+#    print('verhnee pravo')
 
-    if point2[0] == point1[0] + 2 and point2[1] == point1[1] + 1:
-        print('praviy verh')
+# if point2[0] == point1[0] - 1 and point2[1] == point1[1] + 2:
+#  print('verhnee levo')
 
-    if point2[0] == point1[0] + 2 and point2[1] == point1[1] - 1:
-        print('praviy niz')
+# if point2[0] == point1[0] + 2 and point2[1] == point1[1] + 1:
+#  print('praviy verh')
 
-    if point2[0] == point1[0] - 2 and point2[1] == point1[1] - 1:
-        print('niznie levo')
+#   if point2[0] == point1[0] + 2 and point2[1] == point1[1] - 1:
+#      print('praviy niz')
 
-    if point2[0] == point1[0] + 1 and point2[1] == point1[1] - 2:
-        print('niznie pravo')
+# if point2[0] == point1[0] - 2 and point2[1] == point1[1] - 1:
+#    print('niznie levo')
 
-    if point2[0] == point1[0] - 2 and point2[1] == point1[1] + 1:
-        print('leviy verh')
+# if point2[0] == point1[0] + 1 and point2[1] == point1[1] - 2:
+#    print('niznie pravo')
 
-    if point2[0] == point1[0] - 2 and point2[1] == point1[1] - 1:
-        print('leviy niz')
+#  if point2[0] == point1[0] - 2 and point2[1] == point1[1] + 1:
+#    print('leviy verh')
+
+# if point2[0] == point1[0] - 2 and point2[1] == point1[1] - 1:
+# print('leviy niz')
 
 
 with open('INPUT.TXT', 'r') as input:
     with open('OUTPUT.TXT', 'w') as output:
-        output.write(hodKonem(input.readline()))
+        output.write(hodKonem(input.readline().strip()))
